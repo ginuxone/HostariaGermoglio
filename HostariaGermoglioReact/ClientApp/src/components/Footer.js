3e5f997a23a18fa09d53306330d45ea20d3a7f2b
ginuxone <ginito_fox@hotmail.com>
2018-12-05 04:47:20 +0100

    Added base Footer with relative CSS

---
+++ HostariaGermoglioReact/ClientApp/src/components/Footer.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+import './css/Footer.css';
+
+export class Footer extends Component {
+    displayName = Footer.name;
+    
+    render() {
+        return (
+            <Footer>
+                <h3>
+                    @{new Date().getFullYear()}Hostaria Germoglio
+                </h3>
+            </Footer>
+        );
+    }
+}
